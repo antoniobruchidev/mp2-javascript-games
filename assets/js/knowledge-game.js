74c12d8f0fb1a911d529a61965f68163c696e56a
Antonio Bruchi <antoniobruchi@hotmail.com>
2024-04-17 17:42:40 +0100

    Feat: created set of functions that retrieve the definition of every word in the hangman.words array and sets them in the hangman.definitions object with the relative word as the key

--- assets/js/knowledge-game.js
+++ assets/js/knowledge-game.js
@@ -17,6 +17,43 @@ const wordsOptions = {
 		'X-RapidAPI-Host': 'a-randomizer-data-api.p.rapidapi.com'
 	}
 };
+/* Api url for retrieving a single definition. Needs to be added the actual word */
+const definitionUrl = (word) => `https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=${word}`;
+const definitionOptions = {
+   method: 'GET',
+   headers: {
+       'X-RapidAPI-Key': '4a1345c74amsh683f3f88b8c7adfp16be37jsn9e6b2f2a98af',
+       'X-RapidAPI-Host': 'twinword-word-graph-dictionary.p.rapidapi.com'
+   }
+};
+
+/**
+ * Function that iterates through the hangman.words array and call the getDefinition function for every word via a setTimeout so it doen't make too many calls in a second
+ */
+const getDefinitions = () => {
+    let words = hangman.words;
+    for (let i = 0; i < words.length; i++) {
+        setTimeout(function(words, i) {
+            getDefinition(words[i]);
+        }, 1000, words, i);
+    }
+}
+
+/**
+ * Asynchronous function that retrieve the definition of a given word, destructures the response and passes it to hangman.definitions with the word as the key
+ * @param {string} word 
+ */
+const getDefinition = async (word) => {
+        try {
+            const response = await fetch(definitionUrl(word), definitionOptions);
+            const result = await response.json();
+            const { meaning } = result;
+            const { noun, verb, adjective, adverb } = meaning
+            hangman.definitions[word] = { noun, verb, adjective, adverb };
+        } catch (error) {
+            console.error(error);
+        } 
+}
 
 /**
  * Setter function for the array of words in the hangman object.
@@ -30,7 +67,7 @@ const setWords = (words) => {
 }
 
 /**
- * @returns {array} 10 random words
+ * Retrieve 10 random words and call the setter function passing the words as parameter
  */
 const getWords = async () => {
     try {
